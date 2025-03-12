98bded575f5309bf63d3b95aa5453cc85ebbbeda
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 00:54:52 +0800

    Used not in function to check for duplicates and not displaying them

--- no_duplicates.py
+++ no_duplicates.py
@@ -1,9 +1,24 @@
-#create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 #create an Empty list for storing numbers
-numbers = []
+inputed_numbers = []
 #ask for user input
 for i in range(10):
-    num = int(input("Enter Number: "))
-    numbers.append(num)
-    
+    number = int(input("Enter Number: "))
+    inputed_numbers.append(number)
+#checking for duplicates
+recorded = []
+duplicates = []
+for number in inputed_numbers:
+    if number not in recorded:
+        recorded.append(number)  #add the number to 'recorded' if it hasn't been recorded before
+    else:
+        if number not in duplicates:
+            duplicates.append(number)  #add to 'duplicates' if it has appeared before
+#displaying the numbers that don't have duplicates
+print("Numbers that don't have duplicates:") 
+for number in recorded:
+    if number not in duplicates:
+        print(number)
+
+
