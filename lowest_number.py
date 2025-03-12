59a32a877205307f3cd4d6d0408177bc084b05ce
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 01:34:59 +0800

    Used if condition to check for the lowest number in the list

--- lowest_number.py
+++ lowest_number.py
@@ -3,10 +3,17 @@ inputed_numbers = []
 #used while function to start a loop infinitely asking input
 while True:
     try:
-       user_input = int(input("Please enter a number: "))
-       numbers.append(num)  # Add the number to the list if it's valid
+       numbers = int(input("Please enter a number: "))
+       inputed_numbers.append(numbers)  # Add the number to the list if it's valid
     except ValueError:
         # If conversion fails (invalid input), exit the loop
         print("Invalid input!")
         break
+    #display the lowest one
+if inputed_numbers:
+    lowest = inputed_numbers[0]
+    for numbers in inputed_numbers:
+        if numbers < lowest:
+            lowest = numbers
+    print("The lowest number entered is:", lowest)
 
