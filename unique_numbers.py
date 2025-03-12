813474320d4b7e4a9007c12830f82fae5a934498
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 23:04:49 +0800

    Used list to store numbers but can't seemed to detect duplicate

--- unique_numbers.py
+++ unique_numbers.py
@@ -1,10 +1,12 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
-#Ask for user input
-
+numbers = []
 while True:
     try:
         number = int(input("Enter Number: "))
-
+        if number in numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
     except ValueError:
         break
-#Use while loop for continue ask input
+
