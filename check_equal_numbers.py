01ad6d8935d2ec69e7d00a6058870328194326e9
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 15:24:19 +0800

    Added the user input and using if-else function to check if the numbers are equal

---
+++ check_equal_numbers.py
@@ -0,0 +1,8 @@
+#Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+#Ask for user input
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter Number: "))
+#Check numbers if equal
+if num1 == num2:
+    print('Equal')
+#Print'Equal'
