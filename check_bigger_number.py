6b9f901149988d85f3820bd4152423a2fdd62cf1
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 15:22:12 +0800

    Added checking for bigger number using if-else function

---
+++ check_bigger_number.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 2 numbers. Print the bigger number.
+#Asked for user input
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter Number: "))
+#Checking bigger number and print bigger number
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
