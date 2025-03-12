6e7a9e0063db3be4a227ee409d6250e0f1b55361
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 15:20:14 +0800

    Managed to get the program working as intended

---
+++ product_of_two_numbers.py
@@ -0,0 +1,8 @@
+#Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+#Ask for user input
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter Number: "))
+#Multiply numbers
+result = num1 * num2
+print(result)
+#Print product
