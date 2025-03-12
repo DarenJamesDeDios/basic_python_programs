d89e859171e776ba16a16c6b0470cce9eb12ca3a
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 02:18:33 +0800

    used for loops and if-else function to check for duplicates

--- duplicate_numbers.py
+++ duplicate_numbers.py
@@ -5,4 +5,13 @@ inputed_numbers = []
 for i in range(10):
     num = int(input("Enter number: "))
     inputed_numbers.append(num)
-    
+
+duplicates = []
+recorded = []
+#used for loops and if-else function to check for duplicates
+for num in inputed_numbers:
+    if num in recorded and num not in duplicates:
+        duplicates.append(num)
+    else:
+        recorded.append(num)
+
