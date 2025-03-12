2db6e9df408f9fe200d341e7d30913e156ca08b9
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 20:58:04 +0800

    Fixed the issue where the numbers are not printing out

--- all_odd_numbers.py
+++ all_odd_numbers.py
@@ -1,6 +1,5 @@
 #Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-oddnum = 0
+oddnum = 1
 while oddnum < 100:
-    if oddnum % 2 != 0:
-        print(oddnum)
-        oddnum += 1
+    print(oddnum)
+    oddnum += 2
