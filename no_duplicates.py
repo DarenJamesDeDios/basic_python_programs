be09e0ab78252ab0d58f072ca971ab0b39111937
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 21:37:43 +0800

    Created file for batch 3

---
+++ no_duplicates.py
@@ -0,0 +1,8 @@
+#Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+#Ask for user input
+for i in range(10):
+    num = int(input("Enter Number: "))
+    
+#Compile all numbers
+#Check for duplicates
+#Print numbers that don't have duplicates
