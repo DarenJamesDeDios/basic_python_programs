a5a17875677575c0886111d409fa91b690b1a6a9
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 03:16:21 +0800

    Added user input that continuously ask until invalid input

---
+++ average_of_numbers.py
@@ -0,0 +1,13 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+#initialize list for storing numbers
+inputed_numbers = []
+while True:
+    try:
+       numbers = int(input("Please enter a number: "))
+       inputed_numbers.append(numbers)  #add the number to the list if it's valid
+    except ValueError:
+        #makes it exit if the number is invalid
+        print("Invalid input! Enter an appropriate number please.")
+        break
+
+
