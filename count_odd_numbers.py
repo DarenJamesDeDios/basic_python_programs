c3c994f2497c32a2675a636c499b895a6b393644
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 12:30:51 +0800

    Added ask user input and tried to check for odd numbers but its not working

---
+++ count_odd_numbers.py
@@ -0,0 +1,20 @@
+#Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+#Initialized
+oddnum = 0
+#Ask for user input
+print("Enter 10 Numbers:")
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+num = int(input(""))
+#Count odd numbers
+if num % 2 != 0:
+    oddnum += 1
+    print(oddnum)
+#Print how many
