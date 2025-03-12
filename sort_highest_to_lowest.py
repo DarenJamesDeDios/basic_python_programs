f7e45103779cf876a62166229b4efb4c059e6197
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 03:05:38 +0800

    initialized a list and used .append function to store numbers

--- sort_highest_to_lowest.py
+++ sort_highest_to_lowest.py
@@ -1,8 +1,16 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
-#asking continuously until invalid
+
+numbers = []
+
+#asking for input until an invalid input is entered
 while True:
     try:
-        # Ask user for input
+        
         num = int(input("Enter a number: "))
+        #add the number to the list
+        numbers.append(num)
+        
     except ValueError:
+        
+        print("Invalid input! Exiting.")
         break
