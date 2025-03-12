9a38ee0dcac5b2dd5de3d960393b718bc8083314
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 15:15:53 +0800

    Mange to print out 10 numbers but couldn't add them all together

---
+++ summation_of_numbers.py
@@ -0,0 +1,9 @@
+#Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+#Ask user input 10 numbers
+num = 0
+for i in range(10):
+    num = int(input("Enter Number: "))
+    num += num
+print(num)
+#Sum all the numbers
+#Print result
