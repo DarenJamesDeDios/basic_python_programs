8fa8cb8137867b73a5e2b700094ebd3df16efb49
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 01:08:27 +0800

    Used not in function to check the numbers that only appeared for the first time

--- first_entry_duplicate.py
+++ first_entry_duplicate.py
@@ -1,8 +1,15 @@
 #Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
-#Ask for user input
+#initialized for the compilation of numbers
+inputed_numbers = []
+#ask for user input
 for i in range(10):
-    num = int(input("Enter Number: "))
-    
-#Compile All numbers
-#Check for duplicates
-#Print all numbers only for first time but not for second time
+    number = int(input("Enter Number: "))
+    inputed_numbers.append(number)
+#another list for checking duplicate purposes, keeping track of the numbers
+numbers_displayed = []
+#displaying the numbers 
+print("Numbers displayed (only first entry counts):")
+for number in inputed_numbers:
+    if number not in numbers_displayed:
+        print(number)
+        numbers_displayed.append(number)
