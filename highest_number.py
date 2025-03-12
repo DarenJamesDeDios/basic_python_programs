2cd358be5e1d93274a9b01a5d3dba34454b820e3
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 02:52:53 +0800

    Added user input that continuously ask input until user input invalid

---
+++ highest_number.py
@@ -0,0 +1,10 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+inputed_numbers = []
+while True:
+    try:
+       numbers = int(input("Please enter a number: "))
+       inputed_numbers.append(numbers)  #add the number to the list if it's valid
+    except ValueError:
+        #makes it exit if the number is invalid
+        print("Invalid input!")
+        break
