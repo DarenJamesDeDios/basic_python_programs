8d34e80e5f556bc3a35a40cc6c8e5a8a09991916
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 00:49:21 +0800

    Added some more list for storing numbers and sorting them using if-else function

--- no_duplicates.py
+++ no_duplicates.py
@@ -1,8 +1,9 @@
-#Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-#Ask for user input
+#create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+#create an Empty list for storing numbers
+numbers = []
+#ask for user input
 for i in range(10):
     num = int(input("Enter Number: "))
+    numbers.append(num)
     
-#Compile all numbers
-#Check for duplicates
-#Print numbers that don't have duplicates
