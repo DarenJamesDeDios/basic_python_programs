3596879ff77e22e041bc34dafe0e34c3635c18af
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 20:56:15 +0800

    Manage to print only the remainder of the quotient

---
+++ remainder_only.py
@@ -0,0 +1,5 @@
+#Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter Number: "))
+result = num1 % num2 
+print(result)
