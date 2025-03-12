294f7a97d0f33b777cd531aa77da5f43d1274b5c
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 01:27:43 +0800

    Used try and except function for invalid user input

--- lowest_number.py
+++ lowest_number.py
@@ -1,6 +1,12 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
-#Ask for user input
+inputed_numbers = []
+#used while function to start a loop infinitely asking input
 while True:
-    number = int(input("Enter Number: "))
+    try:
+       user_input = int(input("Please enter a number: "))
+       numbers.append(num)  # Add the number to the list if it's valid
+    except ValueError:
+        # If conversion fails (invalid input), exit the loop
+        print("Invalid input!")
+        break
 
-#Use while loop
