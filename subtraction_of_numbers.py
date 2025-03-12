473be1f999533a9061c2ae9bc48544061f4ae9e5
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 20:52:32 +0800

    Fixed the issue where the it couldn't print the difference of the first number

--- subtraction_of_numbers.py
+++ subtraction_of_numbers.py
@@ -1,7 +1,7 @@
 #Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+#This is the first number
 num1 = int(input("Enter Number: "))
-for i in range(10):
-    num2 = int(input("Enter Number: "))
+for i in range(9):
+    num2 = int(input("Enter Number: ")) #This will served as the remaining numbers
     num1 -= num2
     print(num1)
-    
