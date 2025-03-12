b9c1e6fd9e71fec595333af74194674022a9dd12
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 01:39:11 +0800

    Added try and except function to exit the program

--- lowest_to_highest_number.py
+++ lowest_to_highest_number.py
@@ -1,6 +1,12 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
-#Ask for user input
-while True:
-    number = int(input("Enter Number: "))
+#create a list to store numbers
+inputed_numbers = []
 
-#Use while loop
+while True:
+    try:
+        number = int(input("Enter a number: "))
+        inputed_numbers.append(number)
+        
+    except ValueError:
+        print("Invalid input, Try Again!") #To break the loop 
+        break
