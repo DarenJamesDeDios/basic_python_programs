8a6aac1cb7d5b081337ea3586f7a69468c09e05a
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 13:09:46 +0800

    Added user input

---
+++ quotient_with_decimal.py
@@ -0,0 +1,7 @@
+#Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+#Ask user input
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter Number: "))
+#Divide numbers using /
+
+#Print quotient
