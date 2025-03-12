acb04720a837ab48732838c3de23a3fadfc38982
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 02:14:54 +0800

    Added user input for 10 numbers

--- duplicate_numbers.py
+++ duplicate_numbers.py
@@ -1,3 +1,8 @@
 #Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 #create an empty list to store numbers
 inputed_numbers = []
+
+for i in range(10):
+    num = int(input("Enter number: "))
+    inputed_numbers.append(num)
+    
