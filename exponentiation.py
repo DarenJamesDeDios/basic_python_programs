20251755a8bab5dc8f9959753135015cb8120e6a
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 13:23:25 +0800

    The code seemed to be running smoothly

---
+++ exponentiation.py
@@ -0,0 +1,8 @@
+#Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+#Ask for user input
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter Number: "))
+#The base num1 is raised to the num2
+result = num1 ** num2
+#Print result
+print(result)
