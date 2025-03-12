beede75f782c538ff61f861480efd8eee7bf7996
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-12 13:18:41 +0800

    Manage to work the program as intended

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,8 @@
+#Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+#Ask for user input
+num1 = int(input("Enter Number: "))
+num2 = int(input("Enter NUmber: "))
+#Add numbers
+result = num1 + num2
+#Print sum
+print(result)
