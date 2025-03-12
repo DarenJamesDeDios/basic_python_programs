b3f2f044e85fc4150cc47f8f3fede9605ddf9f61
DyrenJmsu <darenjamesdedios@gmail.com>
2025-03-13 02:34:54 +0800

    Added continous user input until invalid

---
+++ most_duplicate_number.py
@@ -0,0 +1,13 @@
+#Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicat
+numbers = []
+
+#continuously asking for user input
+while True:
+    try:
+        
+        num = int(input("Enter a number: "))
+        numbers.append(num)                  #adds them on the list
+        
+    except ValueError:
+        print("Invalid input! Exiting.")
+        break
